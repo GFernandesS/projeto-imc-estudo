d2721d238832216df2e47b4b66ed8b63156da5be
guisao666 <guilherme-schiavone@hotmail.com>
2020-03-14 12:50:32 -0300

    Atribuindo a função a exibição do IMC

--- js/value.js
+++ js/value.js
@@ -1,14 +1,17 @@
 
 var pacientes = [];
 
-for(let n = 0; n<= 4; n++){
-    pacientes.push(document.querySelector(".paciente" + String(n)));
-    let peso = Number(pacientes[n].querySelector(".info-peso").textContent);;
-    let altura = Number(pacientes[n].querySelector(".info-altura").textContent);
-    pacientes[n].querySelector(".info-imc").textContent = calculaImc(peso, altura);
-}
-
-
 function calculaImc(peso, altura){
     return parseInt(peso/(altura**2));
 }
+
+function exibeImc(pacientes){
+    for(let n = 0; n<= 4; n++){
+        pacientes.push(document.querySelector(".paciente" + String(n)));
+        let peso = Number(pacientes[n].querySelector(".info-peso").textContent);;
+        let altura = Number(pacientes[n].querySelector(".info-altura").textContent);
+        pacientes[n].querySelector(".info-imc").textContent = calculaImc(peso, altura);
+    }
+}
+
+exibeImc(pacientes)
